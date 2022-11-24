0d90276b4017ad334e14d391c6f236379ca64ca3
Cauã Rinaldi <cauarinaldi451@gmail.com>
2022-11-24 15:57:00 -0300

    Small change on env_config.rs

--- versions_server/src/env_config.rs
+++ versions_server/src/env_config.rs
@@ -1,5 +1,6 @@
 use std::env::var;
 
+#[derive(Debug)]
 pub struct EnvConfig {
     pub base_path: String,
     pub host_address: [u8; 4],
@@ -19,13 +20,15 @@ impl EnvConfig {
         let host_address = var("HOST_ADDRESS").unwrap_or_else(|_| "127.0.0.1".to_owned());
         let host_address: Vec<u8> = host_address.split('.').map(|o| o.parse().unwrap()).collect();
         let port = var("PORT").unwrap_or_else(|_| "8000".to_owned()).parse().unwrap();
-        let keep_versions = var("KEEP_VERSIONS").unwrap_or_else(|_| "5".to_owned()).parse().unwrap();
+        let keep_versions = var("KEEP_VERSIONS").unwrap_or_else(|_| "50".to_owned()).parse().unwrap();
 
-        Self {
+        let result = Self {
             base_path,
             host_address: host_address.try_into().unwrap(),
             port,
             keep_versions,
-        }
+        };
+        println!("{:?}", result);
+        result
     }
 }
