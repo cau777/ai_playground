47abd5cec6f18277aa721d7d2498a610dc3e42e0
Cauã Rinaldi <cauarinaldi451@gmail.com>
2023-02-16 11:00:45 -0300

    Fix Digits accuracy

--- client/src/i18n/en/translation.ts
+++ client/src/i18n/en/translation.ts
@@ -6,7 +6,7 @@ export default {
         "instructions": "Draw a single digit from 0 to 9. The program uses AI to try guessing it and shows the results in " +
             "the graph below, according to the probabilities of each number. In case the result is wrong, try rewriting the " +
             "digit a bit different.",
-        "limitations": "The model has a tested accuracy of 85% and usually fails with 7 and 9.",
+        "limitations": "The model has a tested accuracy of 90% and usually fails with 7 and 9.",
         "examples": "Examples:"
     },
     "chess": {
