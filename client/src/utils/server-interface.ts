9a5b32aeeb3cf51437b423d39f510f9e7cc747be
Cauã Rinaldi <cauarinaldi451@gmail.com>
2025-05-30 11:44:57 -0300

    Change url

--- client/src/utils/server-interface.ts
+++ client/src/utils/server-interface.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const ServerUrl = import.meta.env.DEV
     ? "http://localhost:8000"
-    : "https://ai-playground-server.livelybay-b5b6ca38.brazilsouth.azurecontainerapps.io";
+    : "https://aipg-versions-server-oqqcf.ondigitalocean.app/";
 
 export async function digits_eval(array: number[]) {
     let response = await axios.post<number[]>(ServerUrl + "/digits/eval", array, {
